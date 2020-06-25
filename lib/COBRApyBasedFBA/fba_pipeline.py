a3c9ef2590cd410c4c3c552191b1497a46fef462
braceal <braceal@umich.edu>
2020-06-25 18:05:09 -0400

    handling some params for static constructor

--- lib/COBRApyBasedFBA/fba_pipeline.py
+++ lib/COBRApyBasedFBA/fba_pipeline.py
@@ -1,4 +1,5 @@
 import cobra
+import cobrakbase
 
 class FBAPipeline:
     
@@ -13,28 +14,43 @@ class FBAPipeline:
         self.feature_ko_list = []
         self.reaction_ko_list = []
         self.custom_bound_list = []
-        self.target_reaction = "bio1"
+        self.target_reaction = 'bio1'
         self.output_id = None #id of the returning FBA object (string)
     
     @staticmethod
     def fromKBaseParams(params):
         pipeline = FBAPipeline()
+
         # Configure method from params
+        self.is_run_fva = params['fva']
+        self.is_all_reversible = params['all_reversible']
+        self.is_pfba = params['minimize_flux']
+        self.is_single_ko = params['simulate_ko']
+        self.fraction_of_optimum = params['objective_fraction']
+        self.media_supplement_list = params['media_supplement_list']
+        self.feature_ko_list = params['feature_ko_list']
+        self.reaction_ko_list = params['reaction_ko_list']
+        self.custom_bound_list = [] # TODO: doesn't seem to be integrated into UI
+        self.target_reaction = params['target_reaction']
 
-        # TODO: this may be all we need. It should update all
-        #       member vars if the params dict has the same
-        #       key names. Need to find out what the key names are.
-        #       otherwise resort to pipeline.is_run_fva = params[...]
-        pipeline.__dict__.update(params)
+        # TODO: add uptake member vars?
+        #    'max_c_uptake':          0.,
+        #    'max_n_uptake':          0.,
+        #    'max_p_uptake':          0.,
+        #    'max_s_uptake':          0.,
+        #    'max_o_uptake':          0.,
+        #    'default_max_uptake':    0.
 
-        return pipeline
+        self.model = None # TODO: figure this out
     
+        return pipeline
     
     def run(self):
 
         if self.model is None:
             raise ValueError('model must be set before calling run')
 
+        # TODO: what data does this contain? What do we do with it?
         # media_supplement_list => [],
         
         # TODO: only do this if is_single_ko?
@@ -64,18 +80,17 @@ class FBAPipeline:
         if self.is_pfba:
             from cobra.flux_analysis import pfba
             sol = pfba(self.model,
-                   fraction_of_optimum=self.fraction_of_optimum,
-                   objective=None,
-                   reactions=None)
+                       fraction_of_optimum=self.fraction_of_optimum,
+                       objective=None,
+                       reactions=None)
 
         if self.is_run_fva:
-            from cobra.flux_analysis import flux_variability_analysis
-            # objective_fraction = 0.1
-            sol = flux_variability_analysis(self.model,
-                    reaction_list=None,
-                    loopless=False,
-                    fraction_of_optimum=self.fraction_of_optimum,
-                    pfba_factor=None)
+            from cobra.flux_analysis import flux_variability_analysis as fva
+            sol = fva(self.model,
+                      reaction_list=None,
+                      loopless=False,
+                      fraction_of_optimum=self.fraction_of_optimum,
+                      pfba_factor=None)
 
         # TODO: add support for cobra.flux_analysis.geometric_fba ?
         
@@ -83,6 +98,7 @@ class FBAPipeline:
             #Simulate all single gene knockouts
             pass
 
+        # TODO: does self.output_id get set here?
         #return result (KBaseFBABuilder object)
             
         return

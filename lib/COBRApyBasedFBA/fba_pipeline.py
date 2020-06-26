9a1728519aa86a5fc4aaad479e5ce6bfbcdeb38d
braceal <braceal@umich.edu>
2020-06-26 15:52:58 -0400

    added gene knockouts, reversible reactions, custom bounds, custom objective, fba logic, single knockout algorithm and placing data into correct return type

--- lib/COBRApyBasedFBA/fba_pipeline.py
+++ lib/COBRApyBasedFBA/fba_pipeline.py
@@ -1,105 +1,118 @@
+import math
 import cobra
 import cobrakbase
+from cobrakbase.core.kbase_fba_builder import KBaseFBABuilder
 
 class FBAPipeline:
+
+    MAX_BOUND = 1000
     
     def __init__(self):
-        self.is_run_fva = False #fva
-        self.is_all_reversible = False #all_reversible
-        self.is_pfba = False #minimize_flux
-        self.is_single_ko = False #simulate_ko
-        self.model = None #COBRApy model object
+        self.is_run_fva = False # run fva
+        self.is_all_reversible = False # all_reversible
+        self.is_pfba = False # minimize_flux
+        self.is_single_ko = False #s imulate_ko
         self.fraction_of_optimum = 1.0
         self.media_supplement_list = []
-        self.feature_ko_list = []
-        self.reaction_ko_list = []
+        self.feature_ko_list = [] # This knocks out genes (which could knockout reactions)
+        self.reaction_ko_list = [] # This knocks out features
         self.custom_bound_list = []
         self.target_reaction = 'bio1'
-        self.output_id = None #id of the returning FBA object (string)
-    
+        self.output_id = '' # id of the returning FBA object
+        self.workspace = ''
+
     @staticmethod
     def fromKBaseParams(params):
-        pipeline = FBAPipeline()
+        p = FBAPipeline()
 
         # Configure method from params
-        self.is_run_fva = params['fva']
-        self.is_all_reversible = params['all_reversible']
-        self.is_pfba = params['minimize_flux']
-        self.is_single_ko = params['simulate_ko']
-        self.fraction_of_optimum = params['objective_fraction']
-        self.media_supplement_list = params['media_supplement_list']
-        self.feature_ko_list = params['feature_ko_list']
-        self.reaction_ko_list = params['reaction_ko_list']
-        self.custom_bound_list = [] # TODO: doesn't seem to be integrated into UI
-        self.target_reaction = params['target_reaction']
+        p.is_run_fva = params['fva']
+        p.is_all_reversible = params['all_reversible']
+        p.is_pfba = params['minimize_flux']
+        p.is_single_ko = params['simulate_ko']
+        p.fraction_of_optimum = params['objective_fraction']
+        p.media_supplement_list = params['media_supplement_list']
+        p.feature_ko_list = params['feature_ko_list']
+        p.reaction_ko_list = params['reaction_ko_list']
+        p.custom_bound_list = [] # TODO: doesn't seem to be integrated into UI
+        p.target_reaction = params['target_reaction']
+        p.workspace = params['workspace']
 
-        # TODO: add uptake member vars?
+        return p
+
+    def run(self, model, media):
+        """This function mutates model."""
+
+        # Add custom bounds to reactions
+        for rct_id, lb, ub in self.custom_bound_list:
+            # TODO: should we check if rct_id is in the reactions?
+            #       depends on interface is it plain text? then yes
+            rct = self.model.reactions.get_by_id(rct_id)
+            rct.lower_bound, rct.upper_bound = lb, ub
+
+        # If specified, make all reactions reversible
+        if self.is_all_reversible:
+            for rct in self.model.reactions:
+                rct.upper_bound = self.MAX_BOUND
+                rct.lower_bound = self.MAX_BOUND * -1
+
+        # Knockouts
+        cobra.manipulations.delete_model_genes(self.model, self.feature_ko_list)
+        for rct_id in self.reaction_ko_list:
+            # TODO: should we check if rct_id is in the reactions?
+            #       depends on interface is it plain text? then yes
+            rct = self.model.reactions.get_by_id(rct_id)
+            rct.lower_bound, rct.upper_bound = 0, 0
+
+        # Set max uptakes
+        # TODO: loop through media and set max uptakes
+        # TODO: add uptake member vars? Might have to parse smiles. This mutates the model constraints.
+        #       data structure of media: https://narrative.kbase.us/#spec/type/KBaseBiochem.Media
         #    'max_c_uptake':          0.,
         #    'max_n_uptake':          0.,
         #    'max_p_uptake':          0.,
         #    'max_s_uptake':          0.,
         #    'max_o_uptake':          0.,
-        #    'default_max_uptake':    0.
+        #    'default_max_uptake':    0.  # TODO: Ask Chris what this is
 
-        self.model = None # TODO: figure this out
-    
-        return pipeline
-    
-    def run(self):
-
-        if self.model is None:
-            raise ValueError('model must be set before calling run')
-
-        # TODO: what data does this contain? What do we do with it?
-        # media_supplement_list => [],
-        
-        # TODO: only do this if is_single_ko?
-        # TODO: what is difference between feature_ko and reaction_ko?
-        # Knockouts
-        # TODO: this impl probably doesn't work
-        # cobra.manipulations.delete_model_genes(self.model, self.feature_ko_list)
-        # cobra.manipulations.delete_model_genes(self.model, self.reaction_ko_list)
-        
-        # Add custom bounds to reactions
-        for rct_id, lb, ub in self.custom_bound_list:
-            # TODO: should we check if rct_id is in the reactions?
-            reaction = self.model.reactions.get_by_id(rct_id)
-            reaction.lower_bound = lb
-            reaction.upper_bound = ub
-
-        # If requested, make all reactions reversible
-        if self.is_all_reversible:
-            pass # TODO https://cobrapy.readthedocs.io/en/latest/faq.html?highlight=reversibility#How-do-I-change-the-reversibility-of-a-Reaction?
-        
-        #Set objective
-        #converting the input string which is a reaction ID and setting the flux of this reaction as the objective
-        #Optimize and save this objective for FBA solution
-
-        # TODO: can is_pfba and is_run_fva both be True?
+        # Set objective
+        # converting the input string which is a reaction ID and setting the flux of this reaction as the objective
+        # Optimize and save this objective for FBA solution
+        # TODO: may have bugs. test this
+        model.objective = self.target_reaction
 
         if self.is_pfba:
             from cobra.flux_analysis import pfba
-            sol = pfba(self.model,
-                       fraction_of_optimum=self.fraction_of_optimum,
-                       objective=None,
-                       reactions=None)
+            fba_sol = pfba(self.model,
+                           fraction_of_optimum=self.fraction_of_optimum,
+                           objective=None, # TODO: do we need these params?
+                           reactions=None)
+        else:
+            from cobra.flux_analysis import fba
+            fba_sol = model.optimize()
 
         if self.is_run_fva:
             from cobra.flux_analysis import flux_variability_analysis as fva
-            sol = fva(self.model,
-                      reaction_list=None,
-                      loopless=False,
-                      fraction_of_optimum=self.fraction_of_optimum,
-                      pfba_factor=None)
-
-        # TODO: add support for cobra.flux_analysis.geometric_fba ?
+            fva_sol = fva(self.model,
+                          fraction_of_optimum=self.fraction_of_optimum)
         
+        # If specified, simulate all single gene knockouts
         if self.is_single_ko:
-            #Simulate all single gene knockouts
-            pass
+            essential_genes = set()
+            for gene in model.genes:
+                cobra.manipulations.delete_model_genes(model, [gene])
+                sol = model.optimize()
+                if sol.status != 'optimal' or math.isclose(sol.objective.value, 0):
+                    essential_genes.add(gene.id)
+                cobra.manipulation.undelete_model_genes(model, [gene])
+                # TODO: add a getter function in KBaseFBABuilder def gene_esential(gene): query set -> bool
 
-        # TODO: does self.output_id get set here?
-        #return result (KBaseFBABuilder object)
+        fba_builder = KBaseFBABuilder.fromCobra(self.output_id,
+                                                model,
+                                                fba_sol,
+                                                media,
+                                                self.workspace)
+        # TODO: add fva_sol and essential_genes to this object in cobrakbase
             
-        return
+        return fba_builder.build()
         
